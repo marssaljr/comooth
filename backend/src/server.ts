20a9ed52de0a18723ccd20a2909e78a3bac046b0
Marçal Junior <marssaljr@gmail.com>
2022-05-04 21:35:59 -0300

    feat: add morgan for logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,19 @@ import "express-async-errors"
 import express, { NextFunction, Request, Response } from "express"
 import cors from 'cors';
 import { router } from "./routes";
+import morgan from 'morgan'
 
 const app = express();
 
+morgan('tiny')
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(router);
-app.use((error: Error, request: Request, response: Response,
-         next: NextFunction) => {
-           return response.status(400).json({
-             status: "Error",
-             message: error.message,
-           })
-         })
-         app.listen(3000, () => console.log("[INFO] Server running!"));
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  return response.status(400).json({
+    status: "Error",
+    message: error.message,
+  })
+})
+app.listen(3000, () => console.log("[INFO] Server running!"));
